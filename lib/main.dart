91df7478384a75dd51406c671a2b954dcf018d24
Mr-Code-start <vitalybaca92@gmail.com>
2026-06-05 05:57:26 -0500

    feat: setup dependency injection, providers for auth view models, and set login page as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:ferova_clinic_flutter/core/di/dependency_injection.dart';
+import 'package:ferova_clinic_flutter/feature/auth/presentation/login_page.dart';
+import 'package:ferova_clinic_flutter/feature/auth/presentation/login_view_model.dart';
+
+import 'feature/auth/presentation/register_view_model.dart';
 
 void main() {
+  setup(); // ← ESTO ES IMPORTANTE: registrar las dependencias
   runApp(const MainApp());
 }
 
@@ -9,11 +16,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => getIt<LoginViewModel>(),
         ),
+        ChangeNotifierProvider(
+          create: (context) => getIt<RegisterViewModel>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Ferova Clinic',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const LoginPage(),
       ),
     );
   }
