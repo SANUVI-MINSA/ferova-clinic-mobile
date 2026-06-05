f2916faeaae3e1a5df52a89b7cbd68978c245b3e
Mr-Code-start <vitalybaca92@gmail.com>
2026-06-05 01:29:48 -0500

    feat: add methods to save and get token and logout

--- lib/feature/auth/domain/auth_repository.dart
+++ lib/feature/auth/domain/auth_repository.dart
@@ -15,4 +15,16 @@ abstract class AuthRepository {
     required String password,
     required String role,
   });
+
+  // Preguntar al profesor mayta si esta correcto poner estos metodos relacionados al token aca mismo
+
+  // Guardar token después de login
+  Future<void> saveToken(String token);
+
+  // Obtener token guardado
+  Future<String?> getToken();
+
+  // Cerrar sesión
+  Future<void> logout();
+
 }
