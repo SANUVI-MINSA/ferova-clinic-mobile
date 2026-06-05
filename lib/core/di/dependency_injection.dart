d5548979fd954a9efedadb1a58e287c59d582ccc
Mr-Code-start <vitalybaca92@gmail.com>
2026-06-05 05:55:44 -0500

    feat: add dependency injection configuration for auth feature

---
+++ lib/core/di/dependency_injection.dart
@@ -0,0 +1,29 @@
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ferova_clinic_flutter/feature/auth/domain/auth_repository.dart';
+import 'package:ferova_clinic_flutter/feature/auth/presentation/login_view_model.dart';
+import 'package:ferova_clinic_flutter/feature/auth/presentation/register_view_model.dart';
+
+import '../../feature/auth/data/auth_repository_impl.dart';
+import '../../feature/auth/data/auth_service.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  // Services
+  getIt.registerLazySingleton<AuthService>(() => AuthService());
+
+  // Repositories
+  getIt.registerLazySingleton<AuthRepository>(
+        () => AuthRepositoryImpl(authService: getIt<AuthService>()),
+  );
+
+  // ViewModels
+  getIt.registerFactory<LoginViewModel>(
+        () => LoginViewModel(repository: getIt<AuthRepository>()),
+  );
+
+  getIt.registerFactory<RegisterViewModel>(
+        () => RegisterViewModel(repository: getIt<AuthRepository>()),
+  );
+}
