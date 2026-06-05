0cffcbb59f616c55f98ce0a7d4406ece0d61feb3
Mr-Code-start <vitalybaca92@gmail.com>
2026-06-05 13:20:42 -0500

    feat: add password reset methods to AuthRepository

--- lib/feature/auth/domain/auth_repository.dart
+++ lib/feature/auth/domain/auth_repository.dart
@@ -15,6 +15,15 @@ abstract class AuthRepository {
     required String role,
   });
 
+  // Request Code
+  Future<User?> requestCode({required String email});
+
+  // Verify Code
+  Future<User?> verifyCode({required String email, required String code});
+
+  // Reset Password
+  Future<User?> resetPassword({required String email, required String code, required String newPassword});
+
   // Guardar token después de login
   Future<void> saveToken(String token);
 
