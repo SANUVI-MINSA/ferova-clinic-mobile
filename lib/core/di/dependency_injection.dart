18b4fdfa483d7dec38ba3f278fc497fef5c0adbf
Mr-Code-start <vitalybaca92@gmail.com>
2026-06-05 20:39:59 -0500

    feat: register forgot password view models in dependency injection

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -2,13 +2,16 @@ import 'package:get_it/get_it.dart';
 import 'package:ferova_clinic_flutter/feature/auth/domain/auth_repository.dart';
 import 'package:ferova_clinic_flutter/feature/auth/presentation/login/login_view_model.dart';
 import 'package:ferova_clinic_flutter/feature/auth/presentation/register/register_view_model.dart';
-
 import '../../feature/auth/data/auth_repository_impl.dart';
 import '../../feature/auth/data/auth_service.dart';
+import '../../feature/auth/presentation/forgot_password/new_password_view_model.dart';
+import '../../feature/auth/presentation/forgot_password/recovery_password_view_model.dart';
+import '../../feature/auth/presentation/forgot_password/verification_identity_view_model.dart';
 
 final getIt = GetIt.instance;
 
 void setup() {
+  // 1 Auth
   // Services
   getIt.registerLazySingleton<AuthService>(() => AuthService());
 
@@ -25,4 +28,18 @@ void setup() {
   getIt.registerFactory<RegisterViewModel>(
         () => RegisterViewModel(repository: getIt<AuthRepository>()),
   );
+
+  // Forgot Password ViewModels
+  getIt.registerFactory<RecoveryPasswordViewModel>(
+        () => RecoveryPasswordViewModel(repository: getIt<AuthRepository>()),
+  );
+
+  getIt.registerFactory<VerificationIdentityViewModel>(
+        () => VerificationIdentityViewModel(repository: getIt<AuthRepository>()),
+  );
+
+  getIt.registerFactory<NewPasswordViewModel>(
+        () => NewPasswordViewModel(repository: getIt<AuthRepository>()),
+  );
+
 }
