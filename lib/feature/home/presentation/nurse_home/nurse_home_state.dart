34ee3a002324047b236d6cf516bf1d50b1c7c818
Mr-Code-start <vitalybaca92@gmail.com>
2026-06-08 03:52:40 -0500

    feat: add nurse_home_state.dart to nurse_home presentation

---
+++ lib/feature/home/presentation/nurse_home/nurse_home_state.dart
@@ -0,0 +1,6 @@
+/// Estado del Home de Enfermería
+///
+/// Este estado maneja toda la información que se muestra en la pantalla del nurse:
+/// - Datos del usuario logueado
+/// - Estadísticas de pacientes
+/// - Listas de citas, etc.
