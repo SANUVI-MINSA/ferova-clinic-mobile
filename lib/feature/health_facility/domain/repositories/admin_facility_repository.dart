0fde16bdab2a963675ada07b6ef1d671ace16ca0
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-06-15 12:01:08 -0500

    feat: add assignNurse method to AdminFacilityRepository for nurse assignment functionality

--- lib/feature/health_facility/domain/repositories/admin_facility_repository.dart
+++ lib/feature/health_facility/domain/repositories/admin_facility_repository.dart
@@ -1,3 +1,5 @@
+import 'package:ferova_clinic_flutter/feature/health_facility/data/dtos/nurse_assignment_request_dto.dart';
+import 'package:ferova_clinic_flutter/feature/health_facility/data/dtos/nurse_assignment_response_dto.dart';
 import 'package:ferova_clinic_flutter/feature/health_facility/data/dtos/nurse_availability_response_dto.dart';
 import 'package:ferova_clinic_flutter/feature/health_facility/domain/model/admin_facility.dart';
 import 'package:ferova_clinic_flutter/feature/health_facility/domain/model/nurse.dart';
@@ -8,4 +10,8 @@ abstract class AdminFacilityRepository {
   Future<NurseAvailabilityResponseDto> canRegisterFacility();
 
   Future<List<Nurse>> getAvailableNurses();
+
+  Future<NurseAssignmentResponseDto> assignNurse(
+    NurseAssignmentRequestDto request,
+  );
 }
