06317599c4d669e60906d3a8bbbcee2a1499e6ed
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-06-15 10:19:21 -0500

    feat: add getAvailableNurses method to AdminFacilityRepository

--- lib/feature/health_facility/domain/repositories/admin_facility_repository.dart
+++ lib/feature/health_facility/domain/repositories/admin_facility_repository.dart
@@ -1,8 +1,11 @@
 import 'package:ferova_clinic_flutter/feature/health_facility/data/dtos/nurse_available_response_dto.dart';
 import 'package:ferova_clinic_flutter/feature/health_facility/domain/model/admin_facility.dart';
+import 'package:ferova_clinic_flutter/feature/health_facility/domain/model/nurse.dart';
 
 abstract class AdminFacilityRepository {
   Future<List<AdminFacility>> getHealthFacilities();
 
   Future<NurseAvailableResponseDto> canRegisterFacility();
+
+  Future<List<Nurse>> getAvailableNurses();
 }
