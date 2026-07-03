251d820fb374696f4de24c18db4a64cd4f0a5a45
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-07-03 16:30:58 -0500

    feat: add GetNursePatientsResponseDto for handling nurse patient responses

--- lib/feature/medical_record/data/dtos/get_nurse_patients_response_dto.dart
+++ lib/feature/medical_record/data/dtos/get_nurse_patients_response_dto.dart
@@ -1,21 +1,21 @@
 import 'package:ferova_clinic_flutter/feature/medical_record/data/dtos/patient_dto.dart';
 
-class GetNursePatientsDto {
+class GetNursePatientsResponseDto {
   final bool success;
   final String status;
   final List<PatientDto> patients;
   final int total;
 
-  const GetNursePatientsDto({
+  const GetNursePatientsResponseDto({
     required this.success,
     required this.status,
     required this.patients,
     required this.total,
   });
 
-  factory GetNursePatientsDto.fromJson(Map<String, dynamic> json) {
+  factory GetNursePatientsResponseDto.fromJson(Map<String, dynamic> json) {
     final data = json['data'] as Map<String, dynamic>;
-    return GetNursePatientsDto(
+    return GetNursePatientsResponseDto(
       success: json['success'] as bool,
       status: json['status'] as String,
       patients: (data['patients'] as List<dynamic>)
