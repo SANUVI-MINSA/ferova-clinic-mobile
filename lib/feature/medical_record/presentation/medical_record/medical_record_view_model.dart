af2b9c8a53fb7e53b1c1540f00e481c04545a05d
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-07-04 17:11:14 -0500

    feat: implement postMedicalRecord method for saving patient medical records

--- lib/feature/medical_record/presentation/medical_record/medical_record_view_model.dart
+++ lib/feature/medical_record/presentation/medical_record/medical_record_view_model.dart
@@ -1,3 +1,5 @@
+import 'package:ferova_clinic_flutter/feature/medical_record/domain/model/entities/medical_record.dart';
+import 'package:ferova_clinic_flutter/feature/medical_record/domain/model/value_objects/patient_history.dart';
 import 'package:ferova_clinic_flutter/feature/medical_record/domain/repository/medical_record_repository.dart';
 import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medical_record/medical_record_state.dart';
 import 'package:flutter/material.dart';
@@ -46,6 +48,45 @@ class MedicalRecordViewModel extends ChangeNotifier {
       state = state.copyWith(
         isCheckingRecords: false,
         errorMessage: e.toString(),
+      );
+    }
+    notifyListeners();
+  }
+
+  Future<void> postMedicalRecord({
+    required String patientId,
+    required double weight,
+    required int height,
+    required String consultationReason,
+    required String observations,
+    required List<PatientHistory> patientHistories,
+    required List<String> symptoms,
+  }) async {
+    state = state.copyWith(isSavingRecord: true, saveErrorMessage: null);
+    notifyListeners();
+
+    try {
+      await repository.postMedicalRecord(
+        MedicalRecord(
+          id: '',
+          patientId: patientId,
+          patientName: '',
+          lastUpdated: '',
+          gender: '',
+          weight: weight,
+          height: height,
+          controls: const [],
+          consultationReason: consultationReason,
+          observations: observations,
+          patientHistories: patientHistories,
+          symptoms: symptoms,
+        ),
+      );
+      state = state.copyWith(isSavingRecord: false, saveErrorMessage: null);
+    } catch (e) {
+      state = state.copyWith(
+        isSavingRecord: false,
+        saveErrorMessage: e.toString(),
       );
     }
     notifyListeners();
