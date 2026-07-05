8b183f71a45069c39a3c3018b6ced1bfcf1f4c05
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-07-05 11:26:59 -0500

    feat: update import statement for UpdateMedicalRecordPage and add new page implementation

--- lib/feature/medical_record/presentation/medical_record/medical_record_page.dart
+++ lib/feature/medical_record/presentation/medical_record/medical_record_page.dart
@@ -4,7 +4,7 @@ import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medica
 import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medical_record/medical_record_summary.dart';
 import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medical_record/medical_record_view_model.dart';
 import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medical_record/register_medical_record_page.dart';
-import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medical_record/update_medical_record.dart';
+import 'package:ferova_clinic_flutter/feature/medical_record/presentation/medical_record/update_medical_record_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
